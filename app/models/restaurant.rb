75618f95210b8774c47a371f3cacbb08eebae382
millene prado <millene_vprado@hotmail.com>
2021-08-18 13:39:50 -0300

    add seeds & freeze category constants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORY = %w[chinese italian japonese french belgian].freeze
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORY }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+hashi_oriental = { name: 'Hashi Oriental', address: 'Av. Jamel', phone_number: '62 9999-9999', category: 'japonese' }
+paris_rest = { name: 'Paris Restaurant', address: 'Paulista', phone_number: '11 9999-9999', category: 'french' }
+spoleto = { name: 'Spoleto', address: 'Av. Flamboyant', phone_number: '62 9999-8888', category: 'italian' }
+china_in_box = { name: 'China In Box', address: 'Rua C-222', phone_number: '62 9999-7777', category: 'chinese' }
+domus = { name: 'Domus Leuven', address: 'Av. T-63', phone_number: '62 9999-6666', category: 'belgian' }
+
+[hashi_oriental, paris_rest, spoleto, china_in_box, domus].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
