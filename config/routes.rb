e49a4c6d012b11bf628ea34676191c0750aa7379
millene prado <millene_vprado@hotmail.com>
2021-08-18 15:20:52 -0300

    css properties

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants, except: %i[edit update destroy] do
-    resources :reviews, only: %i[index new create]
+  resources :restaurants, except: %i[index edit update destroy] do
+    resources :reviews, only: %i[new create]
   end
+  
+  root to: 'restaurants#index'
 end
